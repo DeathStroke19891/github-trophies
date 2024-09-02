f3842c6bb6c2003a260209faab5f2440eaeb5db6
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-09-02 13:44:59 +0530

    Add modified index.js

--- index.js
+++ index.js
@@ -1,17 +1,293 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const Web3 = require('web3');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 31337;
 const SECRET = process.env.GITHUB_WEBHOOK_SECRET || 'comedy';
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_ENDPOINT));  // Use your Infura/RPC endpoint
+
+const githubABI = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_approved",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_approved",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "createSpecificTrophy",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "zombieId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "NewTrophy",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isOwner",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ownerOf",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "trophies",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "trophyToOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const contractAddress = '0x0725cCE44D26a1054ebF077C5FF9e48e77815137';
+const github = new web3.eth.Contract(githubABI, contractAddress);
+
+const userAccount = "0xe873609364131838b5429255ebe7d9885b19ea6a";  // Update with the deployer's account address
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!')
+});
 
 app.use(bodyParser.json());
 
+// GitHub Webhook signature validation
 function validateGitHubSignature(req, res, next) {
     const signature = req.headers['x-hub-signature-256'];
     if (!signature) {
@@ -29,18 +305,46 @@ function validateGitHubSignature(req, res, next) {
     next();
 }
 
-app.post('/', validateGitHubSignature, (req, res) => {
+app.post('/', validateGitHubSignature, async (req, res) => {
     const event = req.headers['x-github-event'];
     const payload = req.body;
 
     console.log(`Received event: ${event}`);
-    const commits = payload.commits;
-    const pusher = payload.pusher;
-  console.log(commits.length);
-    console.log(pusher);
-    res.status(200).send('Webhook received');
+    if (event === 'push') {
+        const commits = payload.commits;
+        const pusher = payload.pusher.name;
+        const commitCount = commits.length;
+
+        try {
+            // Interact with the smart contract
+            await addCommits(userAccount, commitCount);
+            console.log(`Added ${commitCount} commits for user: ${pusher}`);
+            res.status(200).send('Webhook and commit count processed');
+        } catch (error) {
+            console.error("Error processing commits:", error);
+            res.status(500).send('Error processing commit data');
+        }
+    } else {
+        res.status(400).send('Event not supported');
+    }
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// Web3.js function to add commits to the contract
+async function addCommits(owner, number) {
+    const accounts = await web3.eth.getAccounts();
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasEstimate = await github.methods.addCommits(owner, number).estimateGas({ from: accounts[0] });
+
+    return github.methods.addCommits(owner, number)
+        .send({ from: accounts[0], gas: gasEstimate, gasPrice })
+        .on("receipt", function(receipt) {
+            console.log("Successfully added commits");
+        })
+        .on("error", function(error) {
+            console.error(error);
+        });
+}
